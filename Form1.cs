ab92e1ab9fc4b25e9d23d49f65a2e4e39f42ad65
Six21Pro <cameronmwhistler@gmail.com>
2024-08-21 10:41:50 -0400

    Project finished, tallies are working correctly

--- Form1.cs
+++ Form1.cs
@@ -2,10 +2,11 @@ namespace RockPaperSci
 {
     public partial class Form1 : Form
     {
-        char x, y;
+        char x, y;//used to determine W,L,T tallies
+        String wStr, lStr, tStr;//used to display W,L,T tallies
 
-        int z, q = 0;
-        int W, L, T = 0;
+        int q = 0;//randomized variable
+        int W, L, T = 0;//the tallie counts
         public Form1()
         {
             InitializeComponent();
@@ -16,51 +17,39 @@ namespace RockPaperSci
             var rand = new Random();
             countDown();
             await Task.Delay(4000);
-
-            // while (pictureBox1.Image == Properties.Resources._3) { 
             q = rand.Next(1, 4);
             if (q == 1)
             { y = 'r'; pictureBox1.Image = Properties.Resources.crash; }
             else if (q == 2) { y = 'p'; pictureBox1.Image = Properties.Resources.news; }
             else if (q == 3) { y = 's'; pictureBox1.Image = Properties.Resources.sci; }
-            x = 'r';
+            x = 'r';//This is what the user chose
 
             win(x, y);
-
-
-            // }
-
-            //Form2 form2 = new Form2();
-            //form2.ShowDialog();
         }
 
-        public void win(char x, char y)
+        public void win(char x, char y)//determines the result of the RPS.
         {
-            
-            if (x == y) { T++; }//TIE
-            else if (x == 'r' && y == 'p') { L++; }
-            else if (x == 'r' && y == 's') { W++;
-                label4.Text = W.ToString();
-            }
-            else if (x == 'p' && y == 's') { L++; }
-            else if (x == 'p' && y == 'r') { W++; label4.Text = "W.ToString()"; }
-            else if (x == 's' && y == 'r') { L++; }
-            else if (x == 's' && y == 'p') { W++; label4.Text = W.ToString(); }
+
+            if (x == y) { T++; tStr = T.ToString(); label6.Text = tStr; }//TIE
+            else if (x == 'r' && y == 'p') { L++; lStr = L.ToString(); label5.Text = lStr; }//LOSS
+            else if (x == 'r' && y == 's') { W++; wStr = W.ToString(); label4.Text = wStr; }//WIN
+            else if (x == 'p' && y == 's') { L++; lStr = L.ToString(); label5.Text = lStr; }//LOSS
+            else if (x == 'p' && y == 'r') { W++; wStr = W.ToString(); label4.Text = wStr; }//WIN
+            else if (x == 's' && y == 'r') { L++; lStr = L.ToString(); label5.Text = lStr; }//LOSS
+            else if (x == 's' && y == 'p') { W++; wStr = W.ToString(); label4.Text = wStr; }//WIN
         }
 
-        public async void countDown()
+        public async void countDown()//function displays a countdown before showing the randomized selection of R,P,S
         {
 
             pictureBox1.Image = Properties.Resources._1;
-            await Task.Delay(1000);
-            //Thread.Sleep(3000);
+            await Task.Delay(1000);//image displays for a second
+            //Thread.Sleep(3000);//Freezes the whole window and all functions. No good
             pictureBox1.Image = Properties.Resources._2;
             await Task.Delay(1000);
             //Thread.Sleep(3000);
             pictureBox1.Image = Properties.Resources._3;
             await Task.Delay(1000);
-            //Thread.Sleep(3000);
-            //setQ();
         }
 
 
@@ -70,18 +59,17 @@ namespace RockPaperSci
             countDown();
             await Task.Delay(4000);
 
-            // while (pictureBox1.Image == Properties.Resources._3) { 
             q = rand.Next(1, 4);
             if (q == 1)
             { y = 'r'; pictureBox1.Image = Properties.Resources.crash; }
             else if (q == 2) { y = 'p'; pictureBox1.Image = Properties.Resources.news; }
             else if (q == 3) { y = 's'; pictureBox1.Image = Properties.Resources.sci; }
-            x = 'p';
+            x = 'p';//This is what the user chose
 
-            win(x, y);
+            win(x, y);//user and random choice sent to win function
 
 
-           
+
         }
 
         private async void button3_Click(object sender, EventArgs e)
@@ -90,17 +78,16 @@ namespace RockPaperSci
             countDown();
             await Task.Delay(4000);
 
-            // while (pictureBox1.Image == Properties.Resources._3) { 
             q = rand.Next(1, 4);
             if (q == 1)
             { y = 'r'; pictureBox1.Image = Properties.Resources.crash; }
             else if (q == 2) { y = 'p'; pictureBox1.Image = Properties.Resources.news; }
             else if (q == 3) { y = 's'; pictureBox1.Image = Properties.Resources.sci; }
-            x = 's';
+            x = 's';//This is what the user chose
 
-            win(x, y);
+            win(x, y);//user and random choice sent to win function
 
-           
+
         }
     }
 
